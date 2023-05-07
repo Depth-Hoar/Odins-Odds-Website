541a895ea42529b8312f5162091d57a893704629
Depth-Hoar <44712760+Depth-Hoar@users.noreply.github.com>
2023-05-07 16:18:58 -0600

    now maybe fixed image size on mobile?

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,6 +7,13 @@ import '../assets/sass/main.scss';
 import Footer from './Footer';
 import SideBar from './Sidebar';
 
+// Mobile detection function
+function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
+}
+
 class Layout extends Component {
   constructor(props) {
     super(props);
@@ -19,6 +26,11 @@ class Layout extends Component {
     this.timeoutId = setTimeout(() => {
       this.setState({ isPreloaded: false });
     }, 100);
+
+    // Add is-mobile class if the user is on a mobile device
+    if (isMobile()) {
+      document.body.classList.add('is-mobile');
+    }
   }
 
   componentWillUnmount() {
